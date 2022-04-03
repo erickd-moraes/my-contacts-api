e5f737d0467a13cba19706b0fc611919e87b4540
Erick Moraes <erickd.moraes@gmail.com>
2022-04-02 22:59:47 -0300

    create route to register contacts

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -21,8 +21,27 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {
-    // criar novo registro
+  // criar novo registro
+  async store(request, response) {
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExists = await ContactRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response.status(400).json({ error: 'This e-mail already been taken!' });
+    }
+
+    const contact = await ContactRepository.create({
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   update() {
