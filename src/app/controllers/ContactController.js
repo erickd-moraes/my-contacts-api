32c6360cf3ebdb27a425b0807bc0687856bdb9de
Erick Moraes <erickd.moraes@gmail.com>
2022-04-04 16:00:06 -0300

    create database with tables, connect Node to Postgres and refactor ContactRepository

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -3,7 +3,8 @@ const ContactRepository = require('../repositories/ContactRepository');
 class ContactController {
   // listar todos os registros
   async index(request, response) {
-    const contacts = await ContactRepository.findAll();
+    const { orderBy } = request.query;
+    const contacts = await ContactRepository.findAll(orderBy);
 
     response.json(contacts);
   }
@@ -76,12 +77,6 @@ class ContactController {
   // deletar um registro
   async delete(request, response) {
     const { id } = request.params;
-
-    const contact = await ContactRepository.findById(id);
-
-    if (!contact) {
-      return response.status(404).json({ error: 'User not found' });
-    }
 
     await ContactRepository.delete(id);
     // 204: No Content
--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -1,85 +1,49 @@
-const { v4: uuidv4 } = require('uuid');
-
-// eslint-disable-next-line prefer-const
-let contacts = [
-  {
-    id: uuidv4(),
-    name: 'Érick Moraes',
-    email: 'erick@mail.com',
-    phone: '15999999999',
-    category_id: uuidv4(),
-  },
-  {
-    id: uuidv4(),
-    name: 'Gabriela Caroline',
-    email: 'gabriela@mail.com',
-    phone: '15999999999',
-    category_id: uuidv4(),
-  },
-];
+const db = require('../../database');
 
 class ContactRepository {
-  findAll() {
-    return new Promise((resolve) => {
-      resolve(contacts);
-    });
+  async findAll(orderBy = 'ASC') {
+    const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
+
+    const rows = await db.query(`SELECT * FROM contacts ORDER BY name ${direction}`);
+    return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) => {
-      resolve(contacts.find((contact) => contact.id === id));
-    });
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+    return row;
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) => {
-      resolve(contacts.find((contact) => contact.email === email));
-    });
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
+    return row;
   }
 
-  create({
+  async create({
     name, email, phone, category_id,
   }) {
-    return new Promise((resolve) => {
-      const newContact = {
-        id: uuidv4(),
-        name,
-        email,
-        phone,
-        category_id,
-      };
-
-      contacts.push(newContact);
-
-      resolve(newContact);
-    });
+    const [row] = await db.query(`
+      INSERT INTO contacts(name, email, phone, category_id)
+      VALUES($1, $2, $3, $4)
+      RETURNING *
+    `, [name, email, phone, category_id]);
+    return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
-  }
-
-  update(id, {
+  async update(id, {
     name, email, phone, category_id,
   }) {
-    return new Promise((resolve) => {
-      const updatedContact = {
-        id,
-        name,
-        email,
-        phone,
-        category_id,
-      };
+    const [row] = await db.query(`
+      UPDATE contacts
+      SET name = $1, email = $2, phone = $3, category_id = $4
+      WHERE id = $5
+      RETURNING *
+    `, [name, email, phone, category_id, id]);
+    return row;
+  }
 
-      contacts = contacts.map((contact) => (
-        contact.id === id ? updatedContact : contact
-      ));
-
-      resolve(updatedContact);
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+    return deleteOp;
   }
 }
 
