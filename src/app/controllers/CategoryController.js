4a9f30ddbff54713440350a0f75f53c0c1c648a3
Erick Moraes <erickd.moraes@gmail.com>
2022-04-05 16:29:14 -0300

    create CategoryController and CategoryRepository

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,23 @@
+const CategoryRepository = require('../repositories/CategoryRepository');
+
+class CategoryController {
+  async index(request, response) {
+    const category = await CategoryRepository.findAll();
+
+    response.json(category);
+  }
+
+  async store(request, response) {
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoryRepository.create({ name });
+
+    response.json(category);
+  }
+}
+
+module.exports = new CategoryController();
--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3030, () => {
-  // console.log('Server started at http://localhost:3030');
+app.listen(3333, () => {
+  console.log('Server started at http://localhost:3333');
 });
