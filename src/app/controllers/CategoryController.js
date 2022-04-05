f89c31722afc8ea0506e137d7844e2aa289a4919
Erick Moraes <erickd.moraes@gmail.com>
2022-04-05 16:43:25 -0300

    create route to list, update and delete one category

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -7,6 +7,18 @@ class CategoryController {
     response.json(category);
   }
 
+  async show(request, response) {
+    const { id } = request.params;
+
+    const contact = await CategoryRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    response.json(contact);
+  }
+
   async store(request, response) {
     const { name } = request.body;
 
@@ -17,6 +29,33 @@ class CategoryController {
     const category = await CategoryRepository.create({ name });
 
     response.json(category);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    const contactExists = await CategoryRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contact = await CategoryRepository.update(id, { name });
+
+    response.json(contact);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await CategoryRepository.delete(id);
+    // 204: No Content
+    response.sendStatus(204);
   }
 }
 
